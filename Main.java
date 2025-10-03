f8df1632feef9df85fd5369bb922300071152932
SimeonC2026 <simeon.chew2026@gbwl.org>
2025-10-03 12:43:01 +0000

    1.8 Notes

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+/**
+ * @author Simeon C.
+ * @since 3 Oct 2025
+ * @version 1.0
+ * Notes for topics 1.8-1.15
+ */
 public class Main {
 
+   /**
+    * main METHOD is the entry point of the program
+    * @param INPUT is an array of Strings
+    * @return VOID means NO OUTPUT
+    */
    public static void main(String []args) {
       System.out.println("Hello World");
    }
+
 }
